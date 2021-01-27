36f6be0030051303a83a84b8c2eba2f8ea64d0ce
Crabor <crabor19711211@163.com>
2021-01-27 20:58:47 +0800

    update

--- redis-6.0/src/redis-cli.c
+++ redis-6.0/src/redis-cli.c
@@ -60,8 +60,6 @@
 #include "help.h"
 #include "anet.h"
 #include "ae.h"
-//change
-#include "server.h"
 
 #define UNUSED(V) ((void) V)
 
@@ -165,11 +163,11 @@ int *spectrum_palette;
 int spectrum_palette_size;
 
 /* Dict Helpers */
-//change
-static uint64_t cli_dictSdsHash(const void *key);
-static int cli_dictSdsKeyCompare(void *privdata, const void *key1,
+
+static uint64_t dictSdsHash(const void *key);
+static int dictSdsKeyCompare(void *privdata, const void *key1,
     const void *key2);
-static void cli_dictSdsDestructor(void *privdata, void *val);
+static void dictSdsDestructor(void *privdata, void *val);
 static void dictListDestructor(void *privdata, void *val);
 
 /* Cluster Manager Command Info */
@@ -259,8 +257,7 @@ static struct pref {
 } pref;
 
 static volatile sig_atomic_t force_cancel_loop = 0;
-//change
-static void cli_usage(void);
+static void usage(void);
 static void slaveMode(void);
 char *redisGitSHA1(void);
 char *redisGitDirty(void);
@@ -274,8 +271,8 @@ static long getLongInfoField(char *info, char *field);
  *--------------------------------------------------------------------------- */
 
 uint16_t crc16(const char *buf, int len);
-//change
-static long long cli_ustime(void) {
+
+static long long ustime(void) {
     struct timeval tv;
     long long ust;
 
@@ -284,9 +281,9 @@ static long long cli_ustime(void) {
     ust += tv.tv_usec;
     return ust;
 }
-//change
-static long long cli_mstime(void) {
-    return cli_ustime()/1000;
+
+static long long mstime(void) {
+    return ustime()/1000;
 }
 
 static void cliRefreshPrompt(void) {
@@ -435,12 +432,12 @@ static void parseRedisUri(const char *uri) {
     /* Extract database number. */
     config.dbnum = atoi(curr);
 }
-//change
-static uint64_t cli_dictSdsHash(const void *key) {
+
+static uint64_t dictSdsHash(const void *key) {
     return dictGenHashFunction((unsigned char*)key, sdslen((char*)key));
 }
-//change
-static int cli_dictSdsKeyCompare(void *privdata, const void *key1,
+
+static int dictSdsKeyCompare(void *privdata, const void *key1,
         const void *key2)
 {
     int l1,l2;
@@ -451,8 +448,8 @@ static int cli_dictSdsKeyCompare(void *privdata, const void *key1,
     if (l1 != l2) return 0;
     return memcmp(key1, key2, l1) == 0;
 }
-//change
-static void cli_dictSdsDestructor(void *privdata, void *val)
+
+static void dictSdsDestructor(void *privdata, void *val)
 {
     DICT_NOTUSED(privdata);
     sdsfree(val);
@@ -1454,9 +1451,9 @@ static int parseOptions(int argc, char **argv) {
             sdsfree(config.hostip);
             config.hostip = sdsnew(argv[++i]);
         } else if (!strcmp(argv[i],"-h") && lastarg) {
-            cli_usage();
+            usage();
         } else if (!strcmp(argv[i],"--help")) {
-            cli_usage();
+            usage();
         } else if (!strcmp(argv[i],"-x")) {
             config.stdinarg = 1;
         } else if (!strcmp(argv[i],"-p") && !lastarg) {
@@ -1551,7 +1548,7 @@ static int parseOptions(int argc, char **argv) {
         } else if (!strcmp(argv[i],"--verbose")) {
             config.verbose = 1;
         } else if (!strcmp(argv[i],"--cluster") && !lastarg) {
-            if (CLUSTER_MANAGER_MODE()) cli_usage();
+            if (CLUSTER_MANAGER_MODE()) usage();
             char *cmd = argv[++i];
             int j = i;
             while (j < argc && argv[j][0] != '-') j++;
@@ -1559,7 +1556,7 @@ static int parseOptions(int argc, char **argv) {
             createClusterManagerCommand(cmd, j - i, argv + i + 1);
             i = j;
         } else if (!strcmp(argv[i],"--cluster") && lastarg) {
-            cli_usage();
+            usage();
         } else if ((!strcmp(argv[i],"--cluster-only-masters"))) {
             config.cluster_manager_command.flags |=
                     CLUSTER_MANAGER_CMD_FLAG_MASTERS_ONLY;
@@ -1715,8 +1712,8 @@ static sds readArgFromStdin(void) {
     }
     return arg;
 }
-//change
-static void cli_usage(void) {
+
+static void usage(void) {
     sds version = cliVersion();
     fprintf(stderr,
 "redis-cli %s\n"
@@ -2052,7 +2049,7 @@ static void repl(void) {
                 } else if (argc == 1 && !strcasecmp(argv[0],"clear")) {
                     linenoiseClearScreen();
                 } else {
-                    long long start_time = cli_mstime(), elapsed;
+                    long long start_time = mstime(), elapsed;
 
                     issueCommandRepeat(argc-skipargs, argv+skipargs, repeat);
 
@@ -2066,7 +2063,7 @@ static void repl(void) {
                             " -- dataset changes rolled back");
                     }
 
-                    elapsed = cli_mstime()-start_time;
+                    elapsed = mstime()-start_time;
                     if (elapsed >= 500 &&
                         config.output == OUTPUT_STANDARD)
                     {
@@ -2246,20 +2243,20 @@ typedef struct clusterManagerLink {
 } clusterManagerLink;
 
 static dictType clusterManagerDictType = {
-    cli_dictSdsHash,               /* hash function */
+    dictSdsHash,               /* hash function */
     NULL,                      /* key dup */
     NULL,                      /* val dup */
-    cli_dictSdsKeyCompare,         /* key compare */
+    dictSdsKeyCompare,         /* key compare */
     NULL,                      /* key destructor */
-    cli_dictSdsDestructor          /* val destructor */
+    dictSdsDestructor          /* val destructor */
 };
 
 static dictType clusterManagerLinkDictType = {
-    cli_dictSdsHash,               /* hash function */
+    dictSdsHash,               /* hash function */
     NULL,                      /* key dup */
     NULL,                      /* val dup */
-    cli_dictSdsKeyCompare,         /* key compare */
-    cli_dictSdsDestructor,         /* key destructor */
+    dictSdsKeyCompare,         /* key compare */
+    dictSdsDestructor,         /* key destructor */
     dictListDestructor         /* val destructor */
 };
 
@@ -5139,7 +5136,7 @@ static int clusterManagerCheckCluster(int quiet) {
         result = 0;
         if (do_fix/* && result*/) {
             dictType dtype = clusterManagerDictType;
-            dtype.keyDestructor = cli_dictSdsDestructor;
+            dtype.keyDestructor = dictSdsDestructor;
             dtype.valDestructor = dictListDestructor;
             clusterManagerUncoveredSlots = dictCreate(&dtype, NULL);
             int fixed = clusterManagerFixSlotsCoverage(slots);
@@ -6598,7 +6595,7 @@ static void latencyMode(void) {
         config.interval ? config.interval/1000 :
                           LATENCY_HISTORY_DEFAULT_INTERVAL;
     double avg;
-    long long history_start = cli_mstime();
+    long long history_start = mstime();
 
     /* Set a default for the interval in case of --latency option
      * with --raw, --csv or when it is redirected to non tty. */
@@ -6610,13 +6607,13 @@ static void latencyMode(void) {
 
     if (!context) exit(1);
     while(1) {
-        start = cli_mstime();
+        start = mstime();
         reply = reconnectingRedisCommand(context,"PING");
         if (reply == NULL) {
             fprintf(stderr,"\nI/O error\n");
             exit(1);
         }
-        latency = cli_mstime()-start;
+        latency = mstime()-start;
         freeReplyObject(reply);
         count++;
         if (count == 1) {
@@ -6635,16 +6632,16 @@ static void latencyMode(void) {
         } else {
             if (config.latency_history) {
                 latencyModePrint(min,max,avg,count);
-            } else if (cli_mstime()-history_start > config.interval) {
+            } else if (mstime()-history_start > config.interval) {
                 latencyModePrint(min,max,avg,count);
                 exit(0);
             }
         }
 
-        if (config.latency_history && cli_mstime()-history_start > history_interval)
+        if (config.latency_history && mstime()-history_start > history_interval)
         {
-            printf(" -- %.2f seconds range\n", (float)(cli_mstime()-history_start)/1000);
-            history_start = cli_mstime();
+            printf(" -- %.2f seconds range\n", (float)(mstime()-history_start)/1000);
+            history_start = mstime();
             min = max = tot = count = 0;
         }
         usleep(LATENCY_SAMPLE_RATE * 1000);
@@ -6721,7 +6718,7 @@ static void latencyDistMode(void) {
     long long history_interval =
         config.interval ? config.interval/1000 :
                           LATENCY_DIST_DEFAULT_INTERVAL;
-    long long history_start = cli_ustime();
+    long long history_start = ustime();
     int j, outputs = 0;
 
     struct distsamples samples[] = {
@@ -6763,13 +6760,13 @@ static void latencyDistMode(void) {
 
     if (!context) exit(1);
     while(1) {
-        start = cli_ustime();
+        start = ustime();
         reply = reconnectingRedisCommand(context,"PING");
         if (reply == NULL) {
             fprintf(stderr,"\nI/O error\n");
             exit(1);
         }
-        latency = cli_ustime()-start;
+        latency = ustime()-start;
         freeReplyObject(reply);
         count++;
 
@@ -6782,11 +6779,11 @@ static void latencyDistMode(void) {
         }
 
         /* From time to time show the spectrum. */
-        if (count && (cli_ustime()-history_start)/1000 > history_interval) {
+        if (count && (ustime()-history_start)/1000 > history_interval) {
             if ((outputs++ % 20) == 0)
                 showLatencyDistLegend();
             showLatencyDistSamples(samples,count);
-            history_start = cli_ustime();
+            history_start = ustime();
             count = 0;
         }
         usleep(LATENCY_SAMPLE_RATE * 1000);
@@ -7338,10 +7335,10 @@ void type_free(void* priv_data, void* val) {
 }
 
 static dictType typeinfoDictType = {
-    cli_dictSdsHash,               /* hash function */
+    dictSdsHash,               /* hash function */
     NULL,                      /* key dup */
     NULL,                      /* val dup */
-    cli_dictSdsKeyCompare,         /* key compare */
+    dictSdsKeyCompare,         /* key compare */
     NULL,                      /* key destructor (owned by the value)*/
     type_free                  /* val destructor */
 };
@@ -7931,9 +7928,9 @@ static void LRUTestMode(void) {
         /* Perform cycles of 1 second with 50% writes and 50% reads.
          * We use pipelining batching writes / reads N times per cycle in order
          * to fill the target instance easily. */
-        start_cycle = cli_mstime();
+        start_cycle = mstime();
         long long hits = 0, misses = 0;
-        while(cli_mstime() - start_cycle < LRU_CYCLE_PERIOD) {
+        while(mstime() - start_cycle < LRU_CYCLE_PERIOD) {
             /* Write cycle. */
             for (j = 0; j < LRU_CYCLE_PIPELINE_SIZE; j++) {
                 char val[6];
@@ -8021,15 +8018,15 @@ static void intrinsicLatencyMode(void) {
     long long test_end, run_time, max_latency = 0, runs = 0;
 
     run_time = config.intrinsic_latency_duration*1000000;
-    test_end = cli_ustime() + run_time;
+    test_end = ustime() + run_time;
     signal(SIGINT, intrinsicLatencyModeStop);
 
     while(1) {
         long long start, end, latency;
 
-        start = cli_ustime();
+        start = ustime();
         compute_something_fast();
-        end = cli_ustime();
+        end = ustime();
         latency = end-start;
         runs++;
         if (latency <= 0) continue;
---
+++ redis-6.0/src/t_zset_trim.h
@@ -0,0 +1,121 @@
+#include "sds.h"
+#include "dict.h"
+
+/* Objects encoding. Some kind of objects like Strings and Hashes can be
+ * internally represented in multiple ways. The 'encoding' field of the object
+ * is set to one of this fields for this object. */
+#define OBJ_ENCODING_RAW 0     /* Raw representation */
+#define OBJ_ENCODING_INT 1     /* Encoded as integer */
+#define OBJ_ENCODING_HT 2      /* Encoded as hash table */
+#define OBJ_ENCODING_ZIPMAP 3  /* Encoded as zipmap */
+#define OBJ_ENCODING_LINKEDLIST 4 /* No longer used: old list encoding. */
+#define OBJ_ENCODING_ZIPLIST 5 /* Encoded as ziplist */
+#define OBJ_ENCODING_INTSET 6  /* Encoded as intset */
+#define OBJ_ENCODING_SKIPLIST 7  /* Encoded as skiplist */
+#define OBJ_ENCODING_EMBSTR 8  /* Embedded sds string encoding */
+#define OBJ_ENCODING_QUICKLIST 9 /* Encoded as linked list of ziplists */
+#define OBJ_ENCODING_STREAM 10 /* Encoded as a radix tree of listpacks */
+
+#define LRU_BITS 24
+#define LRU_CLOCK_MAX ((1<<LRU_BITS)-1) /* Max value of obj->lru */
+#define LRU_CLOCK_RESOLUTION 1000 /* LRU clock resolution in ms */
+
+#define OBJ_SHARED_REFCOUNT INT_MAX     /* Global object never destroyed. */
+#define OBJ_STATIC_REFCOUNT (INT_MAX-1) /* Object allocated in the stack. */
+#define OBJ_FIRST_SPECIAL_REFCOUNT OBJ_STATIC_REFCOUNT
+typedef struct redisObject {
+    unsigned type:4;
+    unsigned encoding:4;
+    unsigned lru:LRU_BITS; /* LRU time (relative to global lru_clock) or
+                            * LFU data (least significant 8 bits frequency
+                            * and most significant 16 bits access time). */
+    int refcount;
+    void *ptr;
+} robj;
+
+/* ZSETs use a specialized version of Skiplists */
+typedef struct zskiplistNode {
+    sds ele;
+    double score;
+    struct zskiplistNode *backward;
+    struct zskiplistLevel {
+        struct zskiplistNode *forward;
+        unsigned long span;
+    } level[];
+} zskiplistNode;
+
+typedef struct zskiplist {
+    struct zskiplistNode *header, *tail;
+    unsigned long length;
+    int level;
+} zskiplist;
+
+typedef struct zset {
+    dict *dict;
+    zskiplist *zsl;
+} zset;
+
+/* Sorted sets data type */
+
+/* Input flags. */
+#define ZADD_NONE 0
+#define ZADD_INCR (1<<0)    /* Increment the score instead of setting it. */
+#define ZADD_NX (1<<1)      /* Don't touch elements not already existing. */
+#define ZADD_XX (1<<2)      /* Only touch elements already existing. */
+
+/* Output flags. */
+#define ZADD_NOP (1<<3)     /* Operation not performed because of conditionals.*/
+#define ZADD_NAN (1<<4)     /* Only touch elements already existing. */
+#define ZADD_ADDED (1<<5)   /* The element was new and was added. */
+#define ZADD_UPDATED (1<<6) /* The element already existed, score updated. */
+
+/* Flags only used by the ZADD command but not by zsetAdd() API: */
+#define ZADD_CH (1<<16)      /* Return num of elements added or updated. */
+
+/* Struct to hold an inclusive/exclusive range spec by score comparison. */
+typedef struct {
+    double min, max;
+    int minex, maxex; /* are min or max exclusive? */
+} zrangespec;
+
+/* Struct to hold an inclusive/exclusive range spec by lexicographic comparison. */
+typedef struct {
+    sds min, max;     /* May be set to shared.(minstring|maxstring) */
+    int minex, maxex; /* are min or max exclusive? */
+} zlexrangespec;
+
+zskiplist *zslCreate(void);
+void zslFree(zskiplist *zsl);
+zskiplistNode *zslInsert(zskiplist *zsl, double score, sds ele);
+unsigned char *zzlInsert(unsigned char *zl, sds ele, double score);
+int zslDelete(zskiplist *zsl, double score, sds ele, zskiplistNode **node);
+zskiplistNode *zslFirstInRange(zskiplist *zsl, zrangespec *range);
+zskiplistNode *zslLastInRange(zskiplist *zsl, zrangespec *range);
+double zzlGetScore(unsigned char *sptr);
+void zzlNext(unsigned char *zl, unsigned char **eptr, unsigned char **sptr);
+void zzlPrev(unsigned char *zl, unsigned char **eptr, unsigned char **sptr);
+unsigned char *zzlFirstInRange(unsigned char *zl, zrangespec *range);
+unsigned char *zzlLastInRange(unsigned char *zl, zrangespec *range);
+unsigned long zsetLength(const robj *zobj);
+void zsetConvert(robj *zobj, int encoding);
+void zsetConvertToZiplistIfNeeded(robj *zobj, size_t maxelelen);
+int zsetScore(robj *zobj, sds member, double *score);
+unsigned long zslGetRank(zskiplist *zsl, double score, sds o);
+int zsetAdd(robj *zobj, double score, sds ele, int *flags, double *newscore);
+long zsetRank(robj *zobj, sds ele, int reverse);
+int zsetDel(robj *zobj, sds ele);
+void genericZpopCommand(client *c, robj **keyv, int keyc, int where, int emitkey, robj *countarg);
+sds ziplistGetObject(unsigned char *sptr);
+int zslValueGteMin(double value, zrangespec *spec);
+int zslValueLteMax(double value, zrangespec *spec);
+void zslFreeLexRange(zlexrangespec *spec);
+int zslParseLexRange(robj *min, robj *max, zlexrangespec *spec);
+unsigned char *zzlFirstInLexRange(unsigned char *zl, zlexrangespec *range);
+unsigned char *zzlLastInLexRange(unsigned char *zl, zlexrangespec *range);
+zskiplistNode *zslFirstInLexRange(zskiplist *zsl, zlexrangespec *range);
+zskiplistNode *zslLastInLexRange(zskiplist *zsl, zlexrangespec *range);
+int zzlLexValueGteMin(unsigned char *p, zlexrangespec *spec);
+int zzlLexValueLteMax(unsigned char *p, zlexrangespec *spec);
+int zslLexValueGteMin(sds value, zlexrangespec *spec);
+int zslLexValueLteMax(sds value, zlexrangespec *spec);
+
